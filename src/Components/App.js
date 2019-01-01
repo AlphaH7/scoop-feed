8d434de21ccc7a2fca5bfb6f140e9a20fd53dd65
AlphaH7 <alistier.hea@gmail.com>
2019-01-01 23:54:36 +0530

    DEV: Env setup

---
+++ src/Components/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../sass/main.scss';
+
+
+export default class App extends React.Component {
+  constructor() {
+    super()
+  }
+
+  render() {
+    return (<div className="app-view" key={1}>
+           </div>);
+  }
+
+
+}
